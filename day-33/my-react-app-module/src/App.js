7595e44989975b9b59f3b7ebcc54d9a5e2c0c4a1
Morning <morning@mstars-lab2-06s-iMac.local>
2023-01-02 12:31:24 +0800

    day-33 card duussan react

--- day-33/my-react-app-module/src/App.js
+++ day-33/my-react-app-module/src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+import logo from './up-arrow-icon.svg';
 import './App.css';
+import human from "./human-icon.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App container">
+      <h1 className="bg-dark">Popular Product</h1>
+      <hr></hr>
+      <div className="app-card">
+        <img src="https://cdn-icons-png.flaticon.com/512/1160/1160358.png"/>
+        <div className="app-card-append">
+          <div className="arrow">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>55</p>
+          </div>
+          <p>Haught or Naught</p>
+          <p>Hight-minded or absent-minded?You decide.</p>
+         
+          <div className="submit">
+          <p className="submit-1">Submitted by:</p>
+        
+          <img src={human} className="learn" alt="human" />
+          </div>
+        </div>
+      </div>
+      {/* <header className="App-header">
+     
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +38,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
