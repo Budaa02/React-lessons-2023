74a604f3e19dcef9826109542a362f86896d865e
Morning <morning@mstars-lab2-06s-iMac.local>
2023-01-09 10:59:21 +0800

    day-38 angid hiisen button

---
+++ day-38/src/components/Button.jsx
@@ -0,0 +1,13 @@
+export default function Button() {
+    // function handleClick() {
+    //     alert("You clicked me!");
+    // }
+    return (
+        <button onClick={() => {
+            alert("You clicked me")
+        }}>
+            Click me 
+        </button>
+    )
+}
+
---
+++ day-38/src/components/PlayButton.jsx
@@ -0,0 +1,13 @@
+import ChildButton from "./ChildButton";
+
+export default function PlayButton({movieName}){
+    function handlePlayClick() {
+        alert(`Playing ${movieName}`);
+    }
+    return (
+        <ChildButton onClick={handlePlayClick}>Play "{movieName}"</ChildButton>
+
+    )
+}
+
+
