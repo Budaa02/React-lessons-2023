fa19c84cd624c907ec87b4143150fbb8e691ef26
Morning <morning@mstars-lab2-06s-iMac.local>
2023-01-09 12:18:37 +0800

    day-33 card zassan

---
+++ day-33/my-react-app-module/src/components/ProductFunc.jsx
@@ -0,0 +1,26 @@
+
+function ProductFunc(props) {
+    console.log(props);
+    return (
+        <div>
+            <h1>
+                {props.title}
+            </h1>
+            <div>
+                
+            </div>
+        </div>
+
+    )
+}
+
+
+export default ProductFunc
+
+
+
+
+
+
+
+
