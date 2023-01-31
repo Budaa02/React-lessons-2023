89130fbd31f5115999add425252d5b7baba56baa
Morning <morning@mstars-lab2-06s-iMac.local>
2023-01-31 12:37:16 +0800

    day-52 admin paneld product nemsen

---
+++ admin-panel-project/be/app.js
@@ -0,0 +1,17 @@
+console.log("it is my app.js");
+// import necassary module
+
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+/// condiguration of modules
+const app = express();
+const PORT = 8080;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- admin-panel-project/fe/src/App.js
+++ admin-panel-project/fe/src/App.js
@@ -1,18 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import UsersTable from "./components/UserTable";
+import PersistentDrawerLeft from "./pages/Product";
 import Users from "./pages/Users";
 
 function App() {
+  console.log(0 % 2);
   return (
     <div className="App">
       <h1>Admin Panel Project</h1>
       <Routes>
-        <Route
+        {/* <Route
           path="/users"
           element={<Users />}
-        />
+        /> */}
       </Routes>
+      <PersistentDrawerLeft />
     </div>
   );
 }
