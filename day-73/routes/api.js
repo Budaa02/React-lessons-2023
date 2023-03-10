aa2fe3851f336f29af6efbf7a7ff94fa5a47966a
Budaa02 <tbudaa2@gmail.com>
2023-03-10 12:32:35 +0800

    day-74

--- day-73/routes/api.js
+++ day-73/routes/api.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 const api_router = express.Router();
 
 import { getPopularCategories } from "../services/category-services.js";
@@ -7,6 +7,7 @@ import {
   getChildrenMenus,
   getParentCategories,
 } from "../services/menus-service.js";
+import { getAllProducts, search } from "../services/product-services.js";
 api_router.get("/popular", async (request, response) => {
   const result = await getPopularCategories();
   response.status(200).send(result);
@@ -26,4 +27,13 @@ api_router.get("/menus", async (requset, response) => {
   response.status(200).send(parentMenus);
 });
 
+api_router.get("/search", async (request, response) => {
+  const keyword = request.query.keyword;
+  const result = await search(keyword);
+  response.status(200).send(result);
+});
+api_router.get("/products", async (request, response) => {
+  const result = await getAllProducts();
+  response.status(200).send(result);
+});
 export default api_router;
