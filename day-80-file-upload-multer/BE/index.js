c74b12ff20a6bfee171918d1bd7d8f60df2a3d37
Budaa02 <tbudaa2@gmail.com>
2023-03-22 09:02:29 +0800

    day-81-2collection

--- day-80-file-upload-multer/BE/index.js
+++ day-80-file-upload-multer/BE/index.js
@@ -3,6 +3,7 @@ console.log("Day-80");
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -23,14 +24,32 @@ app.use("/uploads", express.static("uploads"));
 
 app.get("/", (request, response) => {
   response.status(200).send("<h1>Hello Day-80 Filupload Multer</h1>");
+  const testFolder = "./uploads";
+  const imgs = [];
+  fs.readdirSync(testFolder).forEach((file) => {
+    console.log(file);
+    const imgURl = `http://localhost:8080/uploads/${file}`;
+    imgs.push(imgURl);
+  });
+  response.status(200).json({
+    data: imgs,
+  });
 });
 
 app.post("/fileUpload", upload.single("image"), (request, response, next) => {
-  console.log(request.file);
-  response.json({
-    data: [],
+  const testFolder = "./uploads/";
+  const imgs = [];
+  fs.readdirSync(testFolder).forEach((file) => {
+    console.log(file);
+    const fileUrl = `http://localhost:8080/uploads/${file}`;
+    imgs.push(fileUrl);
+  });
+
+  response.status(200).json({
+    data: imgs,
   });
 });
+
 app.listen(PORT, () => {
   console.log(`Express Application is running on http://localhost:${PORT}`);
 });
--- day-80-file-upload-multer/fe/src/components/FileUpload.jsx
+++ day-80-file-upload-multer/fe/src/components/FileUpload.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
+
 export default function FileUpload() {
   const URL = "http://localhost:8080/fileUpload";
-
+  const [imageUrls, setImageUrls] = useState([]);
+  const FILE_URL = "http://localhost:8080/files";
+  // useEffect;
+  async function fetchFiles() {
+    const FETCHED_DATA = await fetch(FILE_URL);
+    const FETCHED_JSON = await FETCHED_DATA.json();
+    setImageUrls(FETCHED_JSON.data);
+  }
+  useEffect(() => {
+    fetchFiles();
+  }, []);
   const handleFileUpload = async (event) => {
     event.preventDefault();
     console.log(event.target.image.files[0]);
     const data = new FormData();
     const files = event.target.image.files;
     data.append("image", files[0]);
+
     const options = {
       method: "POST",
       body: data,
@@ -14,6 +27,7 @@ export default function FileUpload() {
     const FETCHED_DATA = await fetch(URL, options);
     const FETCHED_JSON = await FETCHED_DATA.json();
     console.log(FETCHED_JSON);
+    setImageUrls(FETCHED_JSON.data);
   };
   return (
     // <div>
@@ -35,6 +49,13 @@ export default function FileUpload() {
         name="image"
       />
       <button type="submit">Submit</button>
+
+      {imageUrls.map((img) => (
+        <img
+          src={img}
+          alt="img"
+        />
+      ))}
     </form>
   );
 }
